e2bf398bf91d12f9b0bd6c1d1fcaa342d4e585c3
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2024-06-27 20:19:19 +0200

    feat: vec_reverse

--- jsl_vec.h
+++ jsl_vec.h
@@ -20,7 +20,7 @@ struct jsl_vec {
 #define jsl_vec_may_grow(v)                                                                        \
     (vec_size(v) >= vec_capacity(v) ? (v) = vec_realloc(v, vec_capacity(v) * vec_capacity(v)) : 0)
 
-static void *vec_alloc(size_t capacity, size_t element_size) {
+static inline void *vec_alloc(size_t capacity, size_t element_size) {
     struct jsl_vec *p = (struct jsl_vec *)malloc(sizeof(struct jsl_vec) + capacity * element_size);
     if (!p) abort();
     p->size         = 0;
@@ -30,7 +30,7 @@ static void *vec_alloc(size_t capacity, size_t element_size) {
     return &p->buffer;
 }
 
-static void *vec_realloc(void *ptr, size_t capacity) {
+static inline void *vec_realloc(void *ptr, size_t capacity) {
     struct jsl_vec *p_old = vec_ptr(ptr);
     struct jsl_vec *p     = realloc(p_old, sizeof(struct jsl_vec) + capacity * p_old->element_size);
     if (!p) {
@@ -41,7 +41,21 @@ static void *vec_realloc(void *ptr, size_t capacity) {
     return &p->buffer;
 }
 
-static void vec_free(void *ptr) {
+static inline void vec_reverse(void *ptr) {
+    struct jsl_vec *p   = vec_ptr(ptr);
+    void           *tmp = malloc(p->element_size);
+    if (!tmp) abort();
+    for (size_t i = 0; i < p->size / 2; i++) {
+        void *a = ptr + i * p->element_size;
+        void *b = ptr + (p->size - i - 1) * p->element_size;
+        memcpy(tmp, a, p->element_size);
+        memcpy(a, b, p->element_size);
+        memcpy(b, tmp, p->element_size);
+    }
+    free(tmp);
+}
+
+static inline void vec_free(void *ptr) {
     struct jsl_vec *p = vec_ptr(ptr);
     free(p);
 }
--- jsl_vec.test.c
+++ jsl_vec.test.c
@@ -1,5 +1,5 @@
 #include "jsl_vec.h"
-#include "test.h"
+#include "jsl_test.h"
 
 int main(void) {
     int *vec = vec_alloc(3, sizeof(int));
@@ -21,5 +21,19 @@ int main(void) {
     ASSERT_EQ("pop_equals_3", vec_pop(vec), 3);
     ASSERT_EQ("size_3", vec_size(vec), 3);
     ASSERT_EQ("capacity_9", vec_capacity(vec), 9);
+    vec_reverse(vec);
+    ASSERT_EQ("size_3", vec_size(vec), 3);
+    ASSERT_EQ("capacity_9", vec_capacity(vec), 9);
+    ASSERT_EQ("equals_0", vec[0], 2);
+    ASSERT_EQ("equals_1", vec[1], 1);
+    ASSERT_EQ("equals_2", vec[2], 0);
+    vec_push(vec, 3);
+    vec_reverse(vec);
+    ASSERT_EQ("size", vec_size(vec), 4);
+    ASSERT_EQ("capacity", vec_capacity(vec), 9);
+    ASSERT_EQ("equals", vec[0], 3);
+    ASSERT_EQ("equals", vec[1], 0);
+    ASSERT_EQ("equals", vec[2], 1);
+    ASSERT_EQ("equals", vec[3], 2);
     vec_free(vec);
 }
